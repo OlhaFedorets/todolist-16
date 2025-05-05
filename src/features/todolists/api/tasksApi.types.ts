f881913999ae159463ef3ca6742fd7ccf4c13531
OlhaFedorets <fedoretsolha@gmail.com>
2025-05-05 14:15:32 +0300

    change taskApi

--- src/features/todolists/api/tasksApi.types.ts
+++ src/features/todolists/api/tasksApi.types.ts
@@ -1,17 +1,34 @@
-import type { TaskPriority, TaskStatus } from "@/common/enums/enums"
 
-export type DomainTask = {
-  description: string
-  title: string
-  status: TaskStatus
-  priority: TaskPriority
-  startDate: string
-  deadline: string
-  id: string
-  todoListId: string
-  order: number
-  addedDate: string
-}
+import { z } from "zod"
+import { TaskPriority, TaskStatus } from "@/common/enums"
+
+// export type DomainTask = {
+//   description: string
+//   title: string
+//   status: TaskStatus
+//   priority: TaskPriority
+//   startDate: string
+//   deadline: string
+//   id: string
+//   todoListId: string
+//   order: number
+//   addedDate: string
+// }
+
+export const domainTaskSchema = z.object({
+  description: z.string().nullable(),
+  title: z.string(),
+  status: z.nativeEnum(TaskStatus),
+  priority: z.nativeEnum(TaskPriority),
+  startDate: z.string().nullable(),
+  deadline: z.string().nullable(),
+  id: z.string(),
+  todoListId: z.string(),
+  order: z.number(),
+  addedDate: z.string().datetime({ local: true}),
+})
+
+export type DomainTask = z.infer<typeof domainTaskSchema>
 
 export type GetTasksResponse = {
   error: string | null
