961c92d0f781320252070b6c02cc5ce351a8eee0
OlhaFedorets <fedoretsolha@gmail.com>
2025-05-05 14:15:48 +0300

    change networkError

--- src/common/utils/handleServerNetworkError.ts
+++ src/common/utils/handleServerNetworkError.ts
@@ -1,14 +1,20 @@
 import { setAppErrorAC, setAppStatusAC } from "@/app/app-slice"
 import type { Dispatch } from "@reduxjs/toolkit"
 import axios from "axios"
+import { z } from "zod"
 
-export const handleServerNetworkError = (dispatch: Dispatch, error: unknown) => {
+export const handleServerNetworkError = (dispatch: Dispatch, error: any) => {
+  console.table(error.issues)
   let errorMessage
 
   if (axios.isAxiosError(error)) {
     errorMessage = error.response?.data?.message || error.message
   } else if (error instanceof Error) {
-    errorMessage = `Native error: ${error.message}`
+    if (error instanceof z.ZodError) {
+      console.table(error.issues)
+    } else {
+      errorMessage = `Native error: ${error.message}`
+    }
   } else {
     errorMessage = JSON.stringify(error)
   }
