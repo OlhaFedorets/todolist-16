92dda8bdb60068b57848349ede70ea485b094aaa
OlhaFedorets <fedoretsolha@gmail.com>
2025-05-07 11:27:20 +0300

    change PageNotFound.tsx

--- src/common/components/PageNotFound/PageNotFound.tsx
+++ src/common/components/PageNotFound/PageNotFound.tsx
@@ -1,8 +1,14 @@
 import styles from "./PageNotFound.module.css"
+import { Link } from "react-router"
+import Button from "@mui/material/Button"
+import { Path } from "@/common/routing"
 
 export const PageNotFound = () => (
-  <>
+  <div className={styles.all}>
     <h1 className={styles.title}>404</h1>
     <h2 className={styles.subtitle}>page not found</h2>
-  </>
+    <Button variant={'contained'} component={Link} to={Path.Main} sx={{width: '330px', mt: '20px'}} >
+      Вернуться на главную
+    </Button>
+  </div>
 )
